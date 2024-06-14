e504a4b26bd69066e0dedd1e3fcccb043b8d209d
daisy <muiruridaisy16@gmail.com>
2024-06-14 13:37:40 +0300

    Authentification and Home page

---
+++ client/src/AccountNav.jsx
@@ -0,0 +1,129 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function AccountNav() {
+  const { pathname } = useLocation();
+  let subpage = pathname.split("/")?.[2];
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  function linkClasses(type = null) {
+    let classes = "inline-flex gap-1 py-2 px-6 rounded-full";
+    if (type === subpage) {
+      classes += " bg-primary text-white";
+    } else {
+      classes += "  hover:bg-gray-400 bg-gray-200";
+    }
+    return classes;
+  }
+
+  return (
+    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+      <Link className={linkClasses("profile")} to={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+          />
+        </svg>
+        My Profile
+      </Link>
+      <Link className={linkClasses("bookings")} to={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"
+          />
+        </svg>
+        My Bookings
+      </Link>
+      <Link className={linkClasses("places")} to={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
+          />
+        </svg>
+        Accommodations
+      </Link>
+      <Link className={linkClasses("airlines")} to={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+          />
+        </svg>
+        Airlines
+      </Link>
+      <Link className={linkClasses("users")} to={"/"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        Hotel Bookings
+      </Link>
+      <Link
+        className={linkClasses("airlineBookings")}
+        to={"/"}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+          />
+        </svg>
+        Airline Bookings
+      </Link>
+    </nav>
+  );
+}
---
+++ client/src/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import { Toaster } from "react-hot-toast";
+
+export default function Layout() {
+    return (
+        <div className="py-4 px-8 flex flex-col min-h-screen">
+            <Toaster />
+            <Header />
+            <Outlet />
+        </div>
+    )
+}
